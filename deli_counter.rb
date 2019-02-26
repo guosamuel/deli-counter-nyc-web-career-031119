f2fe376c6735b648445534611c2e06530390a717
Samuel Guo <guosamuel1114@gmail.com>
2019-02-26 14:52:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(deli_line)
+
+  if deli_line.length == 0
+    puts "The line is currently empty."
+  else
+    beginning_sentence = "The line is currently:"
+    list_of_names = []
+    deli_line.each_with_index do |item, index|
+      list_of_names.push("#{index+1}. #{item}")
+    end
+    puts "#{beginning_sentence} #{list_of_names.join(" ")}"
+  end
+  
+end
+
+def take_a_number(deli_line, name)
+  
+end
